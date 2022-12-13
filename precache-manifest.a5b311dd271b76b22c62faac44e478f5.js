1da04112161f0991a23dded061ce16c37e081823
barberemi <barbe.remi25@gmail.com>
2022-12-14 00:14:44 +0100

    Updates

---
+++ precache-manifest.a5b311dd271b76b22c62faac44e478f5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "c21c4df758b63dee11e0",
+    "url": "/popbook/static/css/main.f09b72d5.chunk.css"
+  },
+  {
+    "revision": "c21c4df758b63dee11e0",
+    "url": "/popbook/static/js/main.c9ae46aa.chunk.js"
+  },
+  {
+    "revision": "06024fab3d0f3580b564",
+    "url": "/popbook/static/js/runtime~main.5c7688ab.js"
+  },
+  {
+    "revision": "4a245b8db7f5784b260c",
+    "url": "/popbook/static/css/2.c44f2565.chunk.css"
+  },
+  {
+    "revision": "4a245b8db7f5784b260c",
+    "url": "/popbook/static/js/2.08b11743.chunk.js"
+  },
+  {
+    "revision": "8124f7971fb5ff77b40c673791a2670f",
+    "url": "/popbook/static/media/Blomberg-8MKKZ.8124f797.otf"
+  },
+  {
+    "revision": "2ae64c1e93f398534cf74b13b9e943ad",
+    "url": "/popbook/index.html"
+  }
+];
