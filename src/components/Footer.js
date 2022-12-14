3fef43d0e4a6df380b2c8cc48c062663aff46acd
barberemi <barbe.remi25@gmail.com>
2022-12-14 01:54:36 +0100

    add footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,18 +2,34 @@ import React from 'react'
 import styled from '@emotion/styled'
 
 const Pied = styled.footer`
-  position: absolute;
   bottom: 0;
   width: 100%;
-  height: 60px;
-  line-height: 60px;
   background-color: #f5f5f5;
+  border-top: solid 1px #d3d3d3;
 `
 
 export default function Footer() {
   return (
-    <Pied>
-      <div className="container">Bonjour</div>
+    <Pied className="mt-5">
+      <div className="container">
+        <div className="text-center">
+          <img
+            src={process.env.PUBLIC_URL + '/images/aude-pop.png'}
+            height="60px"
+            width="60px"
+          />
+          <div className="text-muted">
+            <small>
+              Ce site n'appartient pas à l'entreprise Funko et n'est donc pas
+              <br />
+              sponsorisé ni validé par Funko.
+              <br />
+              <br />
+              ©2022 RemCorp. Tous droits réservés.
+            </small>
+          </div>
+        </div>
+      </div>
     </Pied>
   )
 }
